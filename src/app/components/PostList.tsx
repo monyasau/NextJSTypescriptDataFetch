c3b6f598b1b3d996b42f5ee22c8b3ccf027f3e07
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-18 12:28:48 +0100

    Feat: added fetch logic

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,9 +1,18 @@
 import PostCard from "./PostCard";
 
 const PostList=()=>{
+    const getData= async()=>{
+        try{
+            let data  =(await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+            return data;
+        }catch (error){
+            console.error("😕",error)
+        }
+    }
     
     return (
         <div className="grid md:grid-col-2">
+            
             <PostCard title="ssmms" body={"dd"}/>
             <PostCard title="smss" body={"dd"}/>
             <PostCard title="vvv" body={"dd"}/>
